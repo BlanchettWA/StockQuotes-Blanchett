a2a8edae0470569d5407dd4297b14c804626adae
William Blanchett <blanchettwa@g.cofc.edu>
2017-03-15 06:41:08 -0400

    Progress on background Async Task method

--- app/src/main/java/tk/blanchettapps/stockquotes_blanchett/MainActivity.java
+++ app/src/main/java/tk/blanchettapps/stockquotes_blanchett/MainActivity.java
@@ -1,53 +1,107 @@
 package tk.blanchettapps.stockquotes_blanchett;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    Stock dataGrab;
-    String[] stockValues;
+     String[] stockValues;
     boolean grabbingData = false;
+    Button doStuff;
+    EditText userInput;
+    String symbolText;
+    Context appcontext = getApplicationContext();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText)findViewById(R.id.targetSymbol);
+
+        doStuff = (Button)findViewById(R.id.button);
+
+        userInput.addTextChangedListener(new TextWatcher() {
+            @Override
+
+
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {}
+
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if (s.length() > 0)
+                {
+                    symbolText = s.toString();
+                    userInput.setText(symbolText);
+                }
+                else
+                {
+                    userInput.setText(R.string.valueless);
+                }
+            }
+        });
+
+
+        doStuff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+
+            }
+
+        });
+
+
+
+
     }
-
-
-
-
 
 
 
 }
 
-class retreiveStocks extends AsyncTask<String, Void, String[]>
-{
+class rStock extends AsyncTask<String,Void, String[]> {
     @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-    }
+    protected String[] doInBackground(String... params) {
+        Stock toproc = new Stock(params[0]);
+        String[] result = new String[6];
+        try
+        {
+            toproc.load();
+            result[0] = toproc.getSymbol();
+            result[1] = toproc.getName();
+            result[2] = toproc.getLastTradeTime();
+            result[3] = toproc.getLastTradePrice();
+            result[4] = toproc.getChange();
+            result[5] = toproc.getRange();
 
-    @Override
-    protected void onPostExecute(String[] strings) {
-        super.onPostExecute(strings);
-    }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            result = null;
+        }
 
-    @Override
-    protected void onCancelled() {
-        super.onCancelled();
-    }
-
-    @Override
-    protected String[] doInBackground(String... params)
-    {
-
-
-
-
-        return null;
+        return result;
     }
 }
